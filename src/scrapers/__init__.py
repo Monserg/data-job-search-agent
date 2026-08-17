0bb33ec9fe6700caa6e6a713333c98405edd2780
SergeyM <msm72@mail.ru>
2026-08-17 14:30:33 +0300

    Add files via upload

---
+++ src/scrapers/__init__.py
@@ -0,0 +1,30 @@
+from . import (
+    djinni,
+    dou,
+    remoteok,
+    weworkremotely,
+    robota_ua,
+    work_ua,
+    nofluffjobs,
+    justremote,
+    indeed,
+    linkedin_alerts,
+    remotive,
+    himalayas,
+)
+
+# Ім'я в config.yaml -> модуль зі search()
+REGISTRY = {
+    "djinni": djinni,
+    "dou": dou,
+    "remoteok": remoteok,
+    "weworkremotely": weworkremotely,
+    "robota_ua": robota_ua,
+    "work_ua": work_ua,
+    "nofluffjobs": nofluffjobs,
+    "justremote": justremote,
+    "indeed": indeed,
+    "linkedin_alerts": linkedin_alerts,
+    "remotive": remotive,
+    "himalayas": himalayas,
+}

785a85a8957972c4540e323022ca17eadbc40af8
SergeyM <msm72@mail.ru>
2026-09-08 11:13:00 +0300

    Add company pattern to HTML link scraping

--- src/scrapers/dou.py
+++ src/scrapers/dou.py
@@ -8,6 +8,7 @@ from .base import html_link_scrape
 
 BASE_URL = "https://jobs.dou.ua"
 LINK_PATTERN = r"/companies/[^/]+/vacancies/\d+"
+COMPANY_PATTERN = r"/companies/([^/]+)/vacancies/\d+"
 
 
 def search(keywords: list) -> list:
@@ -16,6 +17,7 @@ def search(keywords: list) -> list:
         q = urllib.parse.quote(kw)
         url = f"{BASE_URL}/vacancies/?search={q}"
         all_results.extend(
-            html_link_scrape(url, LINK_PATTERN, BASE_URL, "DOU.ua", kw)
+            html_link_scrape(url, LINK_PATTERN, BASE_URL, "DOU.ua", kw,
+                              company_pattern=COMPANY_PATTERN)
         )
     return all_results
